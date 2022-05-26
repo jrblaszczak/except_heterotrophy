98c2b2e953c0558fbdc71d3040ab8f4aa7cefb1a
jrblaszczak <joanna.r.blaszczak@gmail.com>
2022-05-26 11:49:51 -0600

    Update Fig_autotrophy_map_only.R

--- R/Fig_autotrophy_map_only.R
+++ R/Fig_autotrophy_map_only.R
@@ -5,16 +5,15 @@
 ## load more packages
 lapply(c("plyr","dplyr","ggplot2","cowplot",
          "lubridate","tidyverse", "reshape2",
-         "plotrix", "data.table","ggmap","maps","mapdata",
+         "ggmap","maps","mapdata",
          "ggsn","wesanderson"), require, character.only=T)
 
 
 ## Import Data
 NEP_info <- read.csv("")
 
-
 ## Generate map of NEP
-ggmap(get_stamenmap(bbox=c(-125, 25, -66, 50), zoom = 5, 
+NEP_map_fig <- ggmap(get_stamenmap(bbox=c(-125, 25, -66, 50), zoom = 5, 
                              maptype='toner'))+
     geom_point(data = NEP_info, aes(x = lon, y = lat, 
                                   fill=NEP, size=NEP), shape=21)+
@@ -25,7 +24,7 @@ ggmap(get_stamenmap(bbox=c(-125, 25, -66, 50), zoom = 5,
     scale_size_continuous("Mean annual NEP (units C)")
 
 ## Generate map of P:R
-ggmap(get_stamenmap(bbox=c(-125, 25, -66, 50), zoom = 5, 
+PR_map_fig <- ggmap(get_stamenmap(bbox=c(-125, 25, -66, 50), zoom = 5, 
                     maptype='toner'))+
   geom_point(data = NEP_info, aes(x = lon, y = lat, 
                                 fill=PtoR, size=PtoR), shape=21)+
@@ -34,4 +33,10 @@ ggmap(get_stamenmap(bbox=c(-125, 25, -66, 50), zoom = 5,
   scale_fill_gradient("P:R",
                       low = "blue", high = "red")+
   scale_size_continuous("P:R")
+
+
+
+
+
+
 
